4cd3140ad6250ace89c73fe32b9c504f4eb7c5ef
ohjonah <ohjonah@gmail.com>
2017-08-07 14:33:40 -0700

    Adds Error MiddleWare

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+'use strict';
+
 const express = require('express');
 const debug = require('debug')('cfgram:server');
 
+const errors = require('./lib/error-middleware.js');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(errors);
 
 app.listen(PORT, () => debug(`Server up on PORT: ${PORT}`));
