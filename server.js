c6f7033d2d486741305d17e1db2ac8b3ed1dcdb6
ohjonah <ohjonah@gmail.com>
2017-08-07 16:20:08 -0700

    Adds Auth Router

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
 
 const express = require('express');
 const debug = require('debug')('cfgram:server');
-
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const authRouter = require('./route/auth-router.js');
 const errors = require('./lib/error-middleware.js');
+const Promise = require('bluebird');
 
+dotenv.load();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+mongoose.Promise = Promise;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
+});
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(authRouter);
 app.use(errors);
 
 app.listen(PORT, () => debug(`Server up on PORT: ${PORT}`));
